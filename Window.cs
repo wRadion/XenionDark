49192ad60e6de22b47a481fb492d8748ed57a26f
wRadion <wradion@gmail.com>
2020-03-04 19:30:55 +0100

    [Window]: Added ToggleFullscreen method + unified Line endings (sry for the changelog)

--- Window.cs
+++ Window.cs
@@ -13,13 +13,17 @@ namespace XenionDark
     public class Window : System.Windows.Window
     {
         public static DependencyProperty MaximizedWindowTypeProperty = DependencyProperty.Register("MaximizedWindowType", typeof(MaximizedWindowType), typeof(Window), new PropertyMetadata(MaximizedWindowType.Default));
-        public static DependencyProperty MaximizedBorderBrushProperty = DependencyProperty.Register("MaximizedBorderBrush", typeof(Brush), typeof(Window), new PropertyMetadata(new SolidColorBrush(Color.FromArgb(0,0,0,0))));
+        public static DependencyProperty MaximizedBorderBrushProperty = DependencyProperty.Register("MaximizedBorderBrush", typeof(Brush), typeof(Window), new PropertyMetadata(new SolidColorBrush(Color.FromRgb(0,0,0))));
         public static DependencyProperty MaximizedBorderThicknessProperty = DependencyProperty.Register("MaximizedBorderThickness", typeof(Thickness), typeof(Window), new PropertyMetadata(new Thickness(0)));
 
         public MaximizedWindowType MaximizedWindowType
         {
             get => (MaximizedWindowType)GetValue(MaximizedWindowTypeProperty);
-            set => SetValue(MaximizedWindowTypeProperty, value);
+            set
+            {
+                UpdateMaxHeight(value);
+                SetValue(MaximizedWindowTypeProperty, value);
+            }
         }
         public Brush MaximizedBorderBrush
         {
@@ -33,25 +37,50 @@ namespace XenionDark
         }
 
         public Window ThisWindow => this;
-        public Thickness OffsetBorderThickness
-        {
-            get => new Thickness(7 + MaximizedBorderThickness.Left,
-                7 + MaximizedBorderThickness.Top,
-                7 + MaximizedBorderThickness.Right,
-                7 + MaximizedBorderThickness.Bottom);
-        }
 
         static Window()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(Window), new FrameworkPropertyMetadata(typeof(Window)));
         }
 
-        protected override void OnInitialized(EventArgs e)
+        public override void EndInit()
+        {
+            UpdateMaxHeight(MaximizedWindowType);
+            base.EndInit();
+        }
+
+        private void UpdateMaxHeight(MaximizedWindowType type)
+        {
+            if (type == MaximizedWindowType.Default)
+                MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight - 2;
+            else if (type == MaximizedWindowType.Fullscreen)
+                MaxHeight = double.PositiveInfinity;
+        }
+
+        private void ToggleMaximizedWindowType()
         {
             if (MaximizedWindowType == MaximizedWindowType.Default)
-                MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight - 2;
+                MaximizedWindowType = MaximizedWindowType.Fullscreen;
+            else
+                MaximizedWindowType = MaximizedWindowType.Default;
+        }
 
-            base.OnInitialized(e);
+        public void ToggleFullscreen()
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                ToggleMaximizedWindowType();
+                if (MaximizedWindowType == MaximizedWindowType.Fullscreen)
+                {
+                    WindowState = WindowState.Normal;
+                    WindowState = WindowState.Maximized;
+                }
+            }
+            else
+            {
+                MaximizedWindowType = MaximizedWindowType.Fullscreen;
+                WindowState = WindowState.Maximized;
+            }
         }
     }
 }
