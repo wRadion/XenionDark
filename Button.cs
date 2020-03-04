4a41e1005e152e26f7ed36640405277e55bc3b96
wRadion <wradion@gmail.com>
2020-03-04 20:56:45 +0100

    [Button]: Added Button

---
+++ Button.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace XenionDark
+{
+    public class Button : System.Windows.Controls.Button
+    {
+        static Button()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Button), new FrameworkPropertyMetadata(typeof(Button)));
+        }
+    }
+}
