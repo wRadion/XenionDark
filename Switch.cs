e4c6324bf164316855143677c1050ca9f12ad8ca
wRadion <wradion@gmail.com>
2020-03-07 14:15:17 +0100

    [TextBox]: Added placeholder, [Switch]: Added!

---
+++ Switch.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace XenionDark
+{
+    public class Switch : System.Windows.Controls.Primitives.ToggleButton
+    {
+        static Switch()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Switch), new FrameworkPropertyMetadata(typeof(Switch)));
+        }
+    }
+}
--- TextBox.cs
+++ TextBox.cs
@@ -4,6 +4,14 @@ namespace XenionDark
 {
     public class TextBox : System.Windows.Controls.TextBox
     {
+        public static DependencyProperty PlaceholderProperty = DependencyProperty.Register("Placeholder", typeof(string), typeof(TextBox));
+
+        public string Placeholder
+        {
+            get => (string)GetValue(PlaceholderProperty);
+            set => SetValue(PlaceholderProperty, value);
+        }
+
         static TextBox()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(TextBox), new FrameworkPropertyMetadata(typeof(TextBox)));
