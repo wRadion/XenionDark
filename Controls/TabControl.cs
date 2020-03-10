f8bdee96197efdc553e68b6e1ea5cc80f6dfc5b1
wRadion <wradion@gmail.com>
2020-03-10 14:38:04 +0100

    [TabControl]: Added TabControl

---
+++ Controls/TabControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace XenionDark.Controls
+{
+    public class TabControl : System.Windows.Controls.TabControl
+    {
+        public static DependencyProperty TabPaddingProperty = DependencyProperty.Register("TabPadding", typeof(Thickness), typeof(TabControl), new PropertyMetadata(new Thickness(0)));
+
+        public Thickness TabPadding
+        {
+            get => (Thickness)GetValue(TabPaddingProperty);
+            set => SetValue(TabPaddingProperty, value);
+        }
+
+        static TabControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TabControl), new FrameworkPropertyMetadata(typeof(TabControl)));
+        }
+    }
+}
