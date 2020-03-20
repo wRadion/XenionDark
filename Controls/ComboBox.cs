3b35acebaf485b18175e61ad67b1a46e5d9b54a9
wRadion <wradion@gmail.com>
2020-03-20 15:12:31 +0100

    [ComboBox]: Removed SizeToItems property

--- Controls/ComboBox.cs
+++ Controls/ComboBox.cs
@@ -1,43 +1,12 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
 
 namespace XenionDark.Controls
 {
-    public enum SizeToItems
-    {
-        Manual,
-        Width
-    }
-
-
     public class ComboBox : System.Windows.Controls.ComboBox
     {
-        public static DependencyProperty SizeToItemsProperty = DependencyProperty.Register("SizeToItems", typeof(SizeToItems), typeof(ComboBox));
-
-        public SizeToItems SizeToItems
-        {
-            get => (SizeToItems)GetValue(SizeToItemsProperty);
-            set => SetValue(SizeToItemsProperty, value);
-        }
-
         static ComboBox()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ComboBox), new FrameworkPropertyMetadata(typeof(ComboBox)));
-        }
-
-        protected override void OnChildDesiredSizeChanged(UIElement child)
-        {
-            base.OnChildDesiredSizeChanged(child);
-
-            if (SizeToItems == SizeToItems.Width && child is Border border)
-            {
-                ColumnDefinition col = (ColumnDefinition)Template.FindName("firstColumn", this);
-
-                col.Width = new GridLength(border.ActualWidth - 16);
-                Height = MinHeight;
-            }
-
-            ((ListBox)Template.FindName("listBox", this)).Visibility = Visibility.Collapsed;
         }
     }
 }
